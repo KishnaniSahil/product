d82225c9ad24735ca287040cf2e4070d563b05bf
KishnaniSahil <sahilkishnani37@gmail.com>
2022-08-09 15:39:23 +0530

    All in working updated

--- kite/urls.py
+++ kite/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-      path('home/',views.home),
+      path('home/',views.home,name="home"),
       path('about/',views.about),
       path('insert/',views.insert,name="min"),
       path('match/',views.match,name="add"),
--- kite/views.py
+++ kite/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.db import connection
 
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Hello world</h1>")
+
 def about(request):
     cursor=connection.cursor()
-    cursor.execute("SELECT * from post where pid =3")
+    cursor.execute("SELECT * from post")
+    # cursor.execute("SELECT * from post where price between 10 and 50000")
     columns = [col[0] for col in cursor.description]
     posts =  [
         dict(zip(columns, row))
@@ -15,11 +17,15 @@ def about(request):
     ]
     print(posts)
     context={'keyposts':posts}
-    return render(request,'kite/home.html',context)  
+    return render(request,'kite/home.html',context) 
+
 def insert(request):
     return render(request,'kite/form.html')
+    
 def match(request):
-     Name = request.POST['blogTitle']
-     Summary = request.POST['content']
-     cursor = connection.cursor()
-     cursor.execute("INSERT INTO post (`Name`,`Summary`) VALUES ( %s, %s );", (Name, Summary))     
+    print(request)
+    Name = request.POST['blogTitle']
+    Summary = request.POST['content']
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO post (`Name`,`Summary`) VALUES ( %s, %s );", (Name, Summary))
+    return redirect('/text/about')     
